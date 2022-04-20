39e5b92a7a6e5846c4b9c4f82f6c749d703cd5bb
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-20 20:35:14 +0430

    محاسبه طول کلمات با map

---
+++ 46-Map for length input.py
@@ -0,0 +1,17 @@
+def import_os():
+    """ این تابع برای این هست که دیگه دو ایمپورت و دات سیستم رو ننویسی
+     this function help lazy man :)))) 
+     so you can use this :)))
+     """
+    import os
+    os.system('cls')
+
+import_os()
+
+list_names = ['sina', 'sina sinaiii', 'mina', 'mina tehrani', 'lina', 'lina saraii']
+
+def calc_length_words(list_by_map):
+    return len(list_by_map)
+
+print(list(map(calc_length_words, list_names)))
+
