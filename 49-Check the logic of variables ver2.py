d0b4f0c7f44414caa81abb7c39bff721f2c91448
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-21 13:17:40 +0430

    Add files via upload

---
+++ 49-Check the logic of variables ver2.py
@@ -0,0 +1,31 @@
+from re import A
+
+
+def import_os():
+    """ این تابع برای این هست که دیگه دو ایمپورت و دات سیستم رو ننویسی
+     this function help lazy man :)))) 
+     so you can use this :)))
+     """
+    import os
+    os.system('cls')
+
+import_os()
+
+a = 2
+b = a
+b = 3
+print(a, b)
+
+c = [1,2,3]
+d = c
+d[0] = 5           # The list changes here.
+print( c , '-> C')
+print( d , '-> D')
+
+e = [1,2,3]
+f = e
+f = [4,5,6]        # The list does not change here. Because you changed the whole list.
+print( e , '-> E')
+print( f , '-> F')
+
+
