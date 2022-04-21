3ab1947e14361950f10faa4ccade7bf38f9eeec3
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-21 13:44:28 +0430

    منطق متغیر ها در پایتون برای دیکشنری ها
    این منطق هم شبیه همون منطق لیست ها هست

---
+++ 51-Check the logic of variables ver4.py
@@ -0,0 +1,64 @@
+from re import A
+
+
+def import_os():
+    """ این تابع برای این هست که دیگه دو ایمپورت و دات سیستم رو ننویسی
+     this function help lazy man :)))) 
+     so you can use this :)))
+     """
+    import os
+    os.system('cls')
+
+import_os()
+
+# a = 2
+# b = a
+# b = 3
+# print(a, b)
+
+# c = [1,2,3]
+# d = c
+# d[0] = 5           # The list changes here.
+# print( c , '-> C')
+# print( d , '-> D')
+
+# e = [1,2,3]
+# f = e
+# f = [4,5,6]        # The list does not change here. Because you changed the whole list.
+# print( e , '-> E')
+# print( f , '-> F')
+
+# def custom(a):
+#     a = 2
+#     print(a)
+#     return a
+# num = 100
+# print(num)
+# print(custom(num)) # don't change 
+# print(num)
+
+
+# def custom(list_name):
+#     list_name.append('sina')
+
+# example_list = [1, 2, 3, 4]
+# print(example_list)
+# custom(example_list) # The list change here <---------
+# print(example_list)
+
+my_dict = {
+    'ky1' : 'lale'
+}
+# print(type(my_dict))
+my_str = 'sina'
+# print(type(my_str))
+my_dict['name'] = my_str
+# print(my_dict)
+
+def custom(a):
+    my_str = 'sina'
+    a['bakhsh'] = my_str
+
+print(my_dict)
+custom(my_dict) # The dictionary change here
+print(my_dict)
